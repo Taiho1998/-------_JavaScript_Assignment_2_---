0732416d43cc45b6439474491907c964b2d10bc2
Taiho1998 <tgo12323@gmail.com>
2024-10-06 19:34:33 +0900

    오류 수정 및 주석 추가

--- main.js
+++ main.js
@@ -17,15 +17,16 @@ button.addEventListener('click', function(){
         let resultTxt = ''
 
         for (let i = 0; i < articles.length; i++){
-            console.log(xhr.status)
+            // 만약 데이터 베이스 상으로 null이 포함되어 있을 경우 기본값을 정해
+            // includes 함수가 오류를 발생시키지 않게 함
             let title = articles[i]['title'] || 'No title available';
             let author = articles[i]['author'] || 'Unknown author';
             let publishDate = articles[i]['publishedAt'] || 'Unknown date';
             let url = articles[i]['url'] || '#';
             let content = articles[i]['content'] || 'No content available';
             let image = articles[i]['urlToImage'] || 'default_image.jpg';
-
-            if (title.toLowerCase().includes(keyword.toLowerCase()) || author.toLowerCase().includes(keyword.toLowerCase()) || content.toLowerCase().includes(keyword.toLowerCase()))
+            // 검색어를 입력하고, 대소문자 모두 구분 가능하도록 toLowerCase 이용
+            if (keyword !== '' && (title.toLowerCase().includes(keyword.toLowerCase()) || author.toLowerCase().includes(keyword.toLowerCase()) || content.toLowerCase().includes(keyword.toLowerCase()))){
                 resultTxt += `
                 <div style="margin-bottom: 20px;">
                     <img src="${image}" class="thumbnail">
@@ -38,7 +39,10 @@ button.addEventListener('click', function(){
                     </div>
                 </div>
                 `
-
+            }
+        }
+        if (resultTxt === ''){
+            resultTxt = '<div>검색어가 입력되지 않았거나 존재하지 않습니다.</div>'
         }
         result.innerHTML = resultTxt
     }
