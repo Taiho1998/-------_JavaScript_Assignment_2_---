d24b15a1a160511d47523ad8f4be05289889ca30
Taiho1998 <tgo12323@gmail.com>
2024-10-06 17:24:47 +0900

    keyword 변수로 입력받은 검색어 저장

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 
 
 let button = document.getElementById('searchButton')
+let result = document.getElementById('result')
+let input = document.getElementById('searchInput')
 
 button.addEventListener('click', function(){
     let xhr = new XMLHttpRequest()
+    let keyword = input.value
     xhr.open('get', 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ba24d04380254934968f842b7a8ea184', true)
     xhr.onload = function(){
         let title
