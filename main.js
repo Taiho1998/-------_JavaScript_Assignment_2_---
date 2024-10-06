74ef0cef23a06ebf91dd67fab49f0af8ff4bb3ea
Taiho1998 <tgo12323@gmail.com>
2024-10-06 17:22:11 +0900

    html/css 수정 및 결과창 임시 생성, js 작업 시작, api 변경

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+
+let button = document.getElementById('searchButton')
+
+button.addEventListener('click', function(){
+    let xhr = new XMLHttpRequest()
+    xhr.open('get', 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ba24d04380254934968f842b7a8ea184', true)
+    xhr.onload = function(){
+        let title
+        let author
+        let publishDate
+        let url
+        let description
+        let image
+    }
+    xhr.send()
+})
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 api key: ba24d04380254934968f842b7a8ea184
 
 
-https://newsapi.org/v2/everything?q=tesla&from=2024-09-05&sortBy=publishedAt&apiKey=ba24d04380254934968f842b7a8ea184
+https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ba24d04380254934968f842b7a8ea184
